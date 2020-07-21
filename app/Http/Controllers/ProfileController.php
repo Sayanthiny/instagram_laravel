8c9a8c5c1b80bb1abdd409c9e127a8bdc5213ee1
Sayanthiny <rmsayanthi@gmail.com>
2020-07-21 10:38:43 +0530

    first init

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ProfileController extends Controller
+{
+    public function index(User $user)
+    {
+        return view('profiles.index',compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit',compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $data=request()->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'url'=>'url',
+            'image'=>'',
+
+        ]);
+        
+        $user->profile->update($data);
+        return redirect("/profile/{user->id}");
+
+       // return view('profiles.update',compact('user'));
+    }
+}
